cc76fc4a8ba52eadd3c5b69d50016576da30cdee
prasojopxl <prasojodesigner@gmail.com>
2024-01-21 14:30:40 +0700

    test dev

--- app/api/tokenizer/notif/route.js
+++ app/api/tokenizer/notif/route.js
@@ -1,4 +1,4 @@
-import Midtrans from "midtrans-client";
+import midtransClient from "midtrans-client";
 import {NextResponse} from "next/server"
 import axios from "axios"
 
@@ -10,25 +10,43 @@ const config2 = {
 }
 
 
-let snap = new Midtrans.Snap({
+let apiClient = new midtransClient.Snap({
     isProduction: false,
     serverKey: process.env.NEXT_PUBLIC_SECRET,
     clientKey: process.env.NEXT_PUBLIC_CLIENT
 })
 
-// export async function GET() {
-//     return NextResponse.json("hello")
-// }
+export async function GET() {
+    return NextResponse.json("hello")
+}
 
 
 export async function POST(req) {
     const body = await req.json()
-    axios.post("https://adm.stagingaja.com/api/transactions", {
+
+    apiClient.transaction.notification()
+    const dataById = await axios.get(`https://adm.stagingaja.com/api/transactions?filters[order_id]=${body.order_id}`,config2)
+    .then(res=> {
+        return res.data.data[0].id
+    })
+
+    console.log({dataById})
+
+
+    await axios.put(`https://adm.stagingaja.com/api/transactions/${dataById}`, {
         data: {
-            dataku: body
+            dataku: body,
+            payment_type:body.payment_type,
+            gross_amount:body.gross_amount,
+            transaction_status:body.transaction_status,
+
         }
-    }, config2).then((res) => res.data)
+    }, config2).then((res) => {
+        console.log("Ok...")
+    }).catch((error) => {
+        console.log("ini error di sini",error)
+    })
+
     
-    // console.log({dataku})
-    return NextResponse.json(body)
+    return NextResponse.json("ok")
 }
--- app/api/tokenizer/route.js
+++ app/api/tokenizer/route.js
@@ -1,11 +1,20 @@
 import Midtrans from "midtrans-client";
 import {NextResponse} from "next/server"
+import axios from "axios"
 
 let snap = new Midtrans.Snap({
     isProduction: false,
     serverKey: process.env.NEXT_PUBLIC_SECRET,
     clientKey: process.env.NEXT_PUBLIC_CLIENT
 })
+
+const keyApi2 = process.env.KEY_API2
+const config2 = {
+    headers: {
+        'Authorization': 'Bearer ' + keyApi2
+    }
+}
+
 
 export async function GET() {
     return NextResponse.json("hello")
@@ -29,6 +38,21 @@ export async function POST(req) {
             email: email,            
         }
     }
+    await axios.post(`https://adm.stagingaja.com/api/transactions`,{
+        data: {
+            product: productName,
+            customer_name:fullname,
+            email:email,
+            order_id:id,
+            transaction_status:"pending"
+            // gross_amount:price
+
+        }
+    },config2).then((res) => {
+        console.log("okkk")
+    }).catch((error) => {
+        console.log("error")
+    })
 
     const token = await snap.createTransactionToken(parameter)
     console.log({token})
