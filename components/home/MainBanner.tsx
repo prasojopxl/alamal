fc84cfeab1fff74f03dcf1ae3b6ea2a6372dc291
prasojopxl <prasojodesigner@gmail.com>
2024-01-30 14:25:24 +0700

    add data main banner

--- components/home/MainBanner.tsx
+++ components/home/MainBanner.tsx
@@ -1,17 +1,47 @@
-
+"use client"
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from "react-icons/bs";
 import Image from "next/image";
+import Basecontent from "../basecontent";
+import { useQuery } from "@tanstack/react-query"
+import ErrorNetwork from "../errorNetwork";
+import { getData } from "@/app/utils/services";
 
 function ItemSlide(props: any) {
     return (
-        <div><Image src={props.image} width={1919} height={900} alt="image" /></div>
+        <div>
+            {props.children}
+            <Image src={props.image} width={1919} height={900} alt="image" />
+        </div>
     )
 }
 
 export default function MainBanner() {
+    const getQuery = async () => {
+        return await getData("/main-banners?populate=*")
+    }
+    const query = useQuery({
+        queryKey: ["banner"],
+        queryFn: getQuery
+    })
+    if (query.isLoading) {
+        return (
+            <div className=" relative flex justify-center ">
+                <div className="animate-pulse w-full">
+                    <div className="rounded-sm bg-slate-200 h-[400px] w-full "></div>
+                </div>
+            </div>
+        )
+    }
+
+    if (query.isError) {
+        return (
+            <ErrorNetwork />
+        )
+    }
+
     function NextArrow(props: any) {
         const { onClick } = props;
         return (
@@ -52,10 +82,18 @@ export default function MainBanner() {
     };
     return (
         <div className="relative">
-            <Slider {...settings}>
-                <ItemSlide image="/images/img1.jpeg" />
-                <ItemSlide image="/images/img2.jpeg" />
-            </Slider>
+            <Basecontent>
+                <Slider {...settings}>
+                    {
+                        query.data?.data.data.map((item: any) => {
+                            return (
+                                <ItemSlide key={item.id} image={`${process.env.URL_MEDIA}${item.attributes.image.data.attributes.url}`} >
+                                </ItemSlide>
+                            )
+                        })
+                    }
+                </Slider>
+            </Basecontent>
         </div>
     )
 }
