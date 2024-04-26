bef8647ae074bf07e227ca46ce04d7ef1222730f
prasojopxl <prasojodesigner@gmail.com>
2024-04-26 09:12:49 +0700

    add pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from "@/components/Footer"
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.ico" sizes="any" />
+                <Script strategy="lazyOnload">
+                    {`
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '2072979186392651');
+                        fbq('track', 'PageView');
+                  `}
+                </Script>
+                <noscript>
+                    <img height="1" width="1" style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=2072979186392651&ev=PageView&noscript=1"
+                    />
+                </noscript>
             </head>
             <body suppressHydrationWarning={true} className={inter.className}>
                 <Header />
